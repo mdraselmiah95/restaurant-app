6994d6774e39e3876d451ec9a71b19e6ca226483
mdraselmiah95 <russelroy15@gmail.com>
2022-06-10 00:30:41 +0600

    WIP on Restaurant App Header part

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,6 +12,21 @@ const Header = () => {
           <img src={Logo} className="w-8 object-cover" alt="logo" />
           <p className="text-headingColor text-xl font-bold"> City</p>
         </div>
+
+        <ul className="flex items-center gap-24 ">
+          <li className="text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            Home
+          </li>
+          <li className="text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            Menu
+          </li>
+          <li className="text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            About Us
+          </li>
+          <li className="text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            Service
+          </li>
+        </ul>
       </div>
     </header>
   );
