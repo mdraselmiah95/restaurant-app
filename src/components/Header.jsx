b7a8de1e983b05b147d6b4f244444809d91be7be
mdraselmiah95 <russelroy15@gmail.com>
2022-06-10 00:18:37 +0600

    WIP on Restaurant App

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16 bg-primary">
+      {/* desktop & tablet */}
+    </header>
+  );
 };
 
 export default Header;
