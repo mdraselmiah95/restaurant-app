fcb8c8cc2e4ee29d0f0fe595f7b7ed6a22657c34
mdraselmiah95 <russelroy15@gmail.com>
2022-06-10 00:07:52 +0600

    WIP on Restaurant App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
+import { Header } from "./components";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline text-blue-400">
-        Hello world!
-      </h1>
+    <div className="w-screen h-auto flex flex-col bg-primary">
+      <Header />
     </div>
   );
 };
