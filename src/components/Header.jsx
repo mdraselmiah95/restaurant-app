427a3713c5b6c12f5b3f5202c19715fb74d09ed4
mdraselmiah95 <russelroy15@gmail.com>
2022-06-10 00:27:03 +0600

    WIP on Restaurant App Header part

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
+//Logo
+import Logo from "../img/logo.png";
+
 const Header = () => {
   return (
     <header className="fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16 bg-primary">
       {/* desktop & tablet */}
+      <div className="hidden md:flex w-full h-full items-center justify-between">
+        <div className="flex items-center gap-2">
+          <img src={Logo} className="w-8 object-cover" alt="logo" />
+          <p className="text-headingColor text-xl font-bold"> City</p>
+        </div>
+      </div>
     </header>
   );
 };
