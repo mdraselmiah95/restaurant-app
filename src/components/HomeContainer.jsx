c3a97757651d1ee4a794a50c2447e5c0614fb6dc
mdraselmiah95 <russelroy15@gmail.com>
2022-06-11 00:25:29 +0600

    WIP on Restaurant App Home part

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+//images
+import Delivery from "../img/delivery.png";
+
 const HomeContainer = () => {
   return (
     <section
@@ -11,7 +14,35 @@ const HomeContainer = () => {
           <p className="text-base font-semibold text-orange-500">
             Bike Delivery
           </p>
+          <div className="w-8 h-8 overflow-hidden bg-white rounded-full drop-shadow-xl">
+            <img
+              src={Delivery}
+              className="object-contain w-full h-full"
+              alt="delivery"
+            />
+          </div>
         </div>
+
+        <p className="text-[2.5rem] lg:text-[4.5rem] font-bold tracking-wide text-headingColor">
+          The Fastest Delivery in
+          <span className="text-orange-600 text-[3rem] lg:text-[5rem]">
+            Your City
+          </span>
+        </p>
+
+        <p className="text-base text-textColor text-center md:text-left md:w-[80%]">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima velit
+          eaque fugit distinctio est nam voluptatum architecto, porro iusto
+          deserunt recusandae ipsa minus eos sunt, dolores illo repellat facere
+          suscipit!
+        </p>
+
+        <button
+          type="button"
+          className="w-full px-4 py-2 transition-all duration-100 ease-in-out rounded-lg bg-gradient-to-br from-orange-400 to-orange-500 md:w-auto hover:shadow-lg"
+        >
+          Order Now
+        </button>
       </div>
     </section>
   );
