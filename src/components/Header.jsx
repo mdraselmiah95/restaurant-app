7382767f553bd6567fc6d1a09a9f610af3abcdf3
mdraselmiah95 <russelroy15@gmail.com>
2022-06-11 00:12:26 +0600

    WIP on Restaurant App create Context provider

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -33,6 +33,16 @@ const Header = () => {
     }
   };
 
+  const logout = () => {
+    setIsMenu(false);
+    localStorage.clear();
+
+    dispatch({
+      type: actionType.SET_USER,
+      user: null,
+    });
+  };
+
   return (
     <header className="fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16 bg-primary">
       {/* desktop & tablet */}
@@ -96,7 +106,7 @@ const Header = () => {
               )}
               <p
                 className="flex items-center gap-3 px-4 py-2 text-base transition-all duration-100 ease-in-out cursor-pointer hover:bg-slate-100 text-textColor"
-                // onClick={logout}
+                onClick={logout}
               >
                 Logout <MdLogout />
               </p>
@@ -113,7 +123,7 @@ const Header = () => {
         >
           <MdShoppingBasket className="text-2xl cursor-pointer text-textColor" />
           {cartItems && cartItems.length > 0 && (
-            <div className="absolute flex items-center justify-center w-5 h-5 rounded-full  -top-2 -right-2 bg-cartNumBg">
+            <div className="absolute flex items-center justify-center w-5 h-5 rounded-full -top-2 -right-2 bg-cartNumBg">
               <p className="text-xs font-semibold text-white">
                 {/* {cartItems.length} */}
               </p>
@@ -178,7 +188,7 @@ const Header = () => {
 
               <p
                 className="flex items-center justify-center gap-3 p-2 m-2 text-base transition-all duration-100 ease-in-out bg-gray-200 rounded-md shadow-md cursor-pointer hover:bg-gray-300 text-textColor"
-                // onClick={logout}
+                onClick={logout}
               >
                 Logout <MdLogout />
               </p>
