0a84b7d075b1c444b18702559799e79b8856723b
mdraselmiah95 <russelroy15@gmail.com>
2022-06-10 22:21:36 +0600

    WIP on Restaurant App create Context provider

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
+import { StateProvider } from "./components/context/StateProvider";
+import { initialState } from "./components/context/initalState";
+import reducer from "./components/context/reducer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
-    <App />
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
   </Router>
 );
