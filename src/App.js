cce07393b44b9d5087ad96d182a4f4bdf5221978
mdraselmiah95 <russelroy15@gmail.com>
2022-06-12 18:15:52 +0600

    WIP on Restaurant App Create container page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import { CreateContainer, Header, MainContainer } from "./components";
+import { useStateValue } from "./components/context/StateProvider";
+import { getAllFoodItems } from "./components/utils/firebaseFunctions";
+import { actionType } from "./components/context/reducer";
 
 const App = () => {
+  const [{ foodItems }, dispatch] = useStateValue();
+
+  const fetchData = async () => {
+    await getAllFoodItems().then((data) => {
+      dispatch({
+        type: actionType.SET_FOOD_ITEMS,
+        foodItems: data,
+      });
+    });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <AnimatePresence exitBeforeEnter>
       <div className="flex flex-col w-screen h-auto bg-primary">
--- src/components/context/initalState.js
+++ src/components/context/initalState.js
@@ -4,7 +4,7 @@ const userInfo = fetchUser();
 
 export const initialState = {
   user: userInfo,
-  //   foodItems: null,
+  foodItems: null,
   //   cartShow: false,
   //   cartItems: cartInfo,
 };
