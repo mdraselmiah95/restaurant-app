a6cc8b3771ebfe81574f4c6428e46c50b154844a
mdraselmiah95 <russelroy15@gmail.com>
2022-06-14 22:15:21 +0600

    WIP on Restaurant App Cart container part

--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -8,18 +8,18 @@ const CartContainer = () => {
   return (
     <div className="fixed top-0 right-0 w-full md:w-375 h-screen bg-white drop-shadow-md flex flex-col z-[101]">
       <div className="w-full flex items-center justify-between p-4 cursor-pointer">
-        <div>
+        <motion.div whileTap={{ scale: 0.75 }}>
           <MdOutlineKeyboardBackspace className="text-textColor text-3xl" />
-        </div>
+        </motion.div>
         <p className="text-textColor text-lg font-semibold">Cart</p>
 
-        <p
-          //   whileTap={{ scale: 0.75 }}
+        <motion.p
+          whileTap={{ scale: 0.75 }}
           className="flex items-center gap-2 p-1 px-2 my-2 bg-gray-100 rounded-md hover:shadow-md  cursor-pointer text-textColor text-base"
           //   onClick={clearCart}
         >
           Clear <RiRefreshFill />
-        </p>
+        </motion.p>
       </div>
     </div>
   );
