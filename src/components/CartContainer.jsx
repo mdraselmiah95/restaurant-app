7bcfde5ccd726cf59e3d2eefdba83cb3b7d799c4
mdraselmiah95 <russelroy15@gmail.com>
2022-06-15 00:28:57 +0600

    WIP on Restaurant App Cart Item part

--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -7,7 +7,7 @@ import CartItem from "./CartItem";
 import { useStateValue } from "../context/StateProvider";
 import { actionType } from "../context/reducer";
 
-//Image
+//Images
 import EmptyCart from "../img/emptyCart.svg";
 
 const CartContainer = () => {
@@ -38,43 +38,45 @@ const CartContainer = () => {
         >
           Clear <RiRefreshFill />
         </motion.p>
+      </div>
 
-        {/* bottom section */}
-        {cartItems && cartItems.length > 0 ? (
-          <div className="w-full h-full bg-cartBg rounded-t-[2rem] flex flex-col">
-            {/* cart Items section */}
-            <div className="w-full h-340 md:h-42 px-6 py-10 flex flex-col gap-3 overflow-y-scroll scrollbar-none">
-              {/* cart Item */}
-              {cartItems &&
-                cartItems.length > 0 &&
-                cartItems.map((item) => (
-                  <CartItem
-                    key={item.id}
-                    item={item}
-                    //   setFlag={setFlag}
-                    //   flag={flag}
-                  />
-                ))}
+      {/* bottom section */}
+      {cartItems && cartItems.length > 0 ? (
+        <div className="w-full h-full bg-cartBg rounded-t-[2rem] flex flex-col">
+          {/* cart Items section */}
+          <div className="w-full h-340 md:h-42 px-6 py-10 flex flex-col gap-3 overflow-y-scroll scrollbar-none">
+            {/* cart Item */}
+            {cartItems &&
+              cartItems.length > 0 &&
+              cartItems.map((item) => (
+                <CartItem
+                  key={item.id}
+                  item={item}
+                  //   setFlag={setFlag}
+                  //   flag={flag}
+                />
+              ))}
+          </div>
+
+          {/* cart total section */}
+          <div className="w-full flex-1 bg-cartTotal rounded-t-[2rem] flex flex-col items-center justify-evenly px-8 py-2">
+            <div className="w-full flex items-center justify-between">
+              <p className="text-gray-400 text-lg">Sub Total</p>
+              <p className="text-gray-400 text-lg">$ tot</p>
             </div>
-            {/* cart total section */}
-            <div className="w-full flex-1 bg-cartTotal rounded-t-[2rem] flex flex-col items-center justify-evenly px-8 py-2">
-              <div className="w-full flex items-center justify-between">
-                <p className="text-gray-400 text-lg">Sub Total</p>
-                <p className="text-gray-400 text-lg">$ tot</p>
-              </div>
-              <div className="w-full flex items-center justify-between">
-                <p className="text-gray-400 text-lg">Delivery</p>
-                <p className="text-gray-400 text-lg">$ 2.5</p>
-              </div>
-              <div className="w-full border-b border-gray-600 my-2"></div>
+            <div className="w-full flex items-center justify-between">
+              <p className="text-gray-400 text-lg">Delivery</p>
+              <p className="text-gray-400 text-lg">$ 2.5</p>
+            </div>
 
-              <div className="w-full flex items-center justify-between">
-                <p className="text-gray-200 text-xl font-semibold">Total</p>
-                <p className="text-gray-200 text-xl font-semibold">
-                  $tot + 2.5
-                </p>
-              </div>
+            <div className="w-full border-b border-gray-600 my-2"></div>
 
+            <div className="w-full flex items-center justify-between">
+              <p className="text-gray-200 text-xl font-semibold">Total</p>
+              <p className="text-gray-200 text-xl font-semibold">$tot + 2.5</p>
+            </div>
+
+            {user ? (
               <motion.button
                 whileTap={{ scale: 0.8 }}
                 type="button"
@@ -82,7 +84,7 @@ const CartContainer = () => {
               >
                 Check Out
               </motion.button>
-
+            ) : (
               <motion.button
                 whileTap={{ scale: 0.8 }}
                 type="button"
@@ -90,17 +92,17 @@ const CartContainer = () => {
               >
                 Login to check out
               </motion.button>
-            </div>
+            )}
           </div>
-        ) : (
-          <div className="w-full h-full flex flex-col items-center justify-center gap-6">
-            <img src={EmptyCart} className="w-300" alt="" />
-            <p className="text-xl text-textColor font-semibold">
-              Add some items to your cart
-            </p>
-          </div>
-        )}
-      </div>
+        </div>
+      ) : (
+        <div className="w-full h-full flex flex-col items-center justify-center gap-6">
+          <img src={EmptyCart} className="w-300" alt="" />
+          <p className="text-xl text-textColor font-semibold">
+            Add some items to your cart
+          </p>
+        </div>
+      )}
     </motion.div>
   );
 };
